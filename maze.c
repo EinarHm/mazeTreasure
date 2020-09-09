fc986fbb8b53c460874c826e3b78745f5b8428b3
Einar Hohenstein Mares <41242610+EinarHm@users.noreply.github.com>
2020-09-09 15:12:08 -0500

    Add manualPlay

--- maze.c
+++ maze.c
@@ -17,6 +17,18 @@ int maze[TAM][TAM] = {
     {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
+int playMaze[TAM][TAM] = {
+    {8, 0, 0, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+    {0, 0, 0, 1, 1, 1, 0, 1, 0, 1},
+    {0, 1, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 1, 0, 1, 1, 0, 1, 0, 1, 0},
+    {0, 0, 1, 1, 1, 0, 1, 0, 1, 0},
+    {1, 0, 0, 0, 0, 1, 1, 0, 1, 0},
+    {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
 int solucion[TAM][TAM];
 
 void printMaze()
@@ -28,6 +40,19 @@ void printMaze()
         for (int j = 0; j < TAM; j++)
         {
             printf("%d\t", maze[i][j]);
+        }
+        printf("\n\n");
+    }
+}
+
+void printPlayerMaze()
+{
+    int i, j;
+    for (int i = 0; i < TAM; i++)
+    {
+        for (int j = 0; j < TAM; j++)
+        {
+            printf("%d\t", playMaze[i][j]);
         }
         printf("\n\n");
     }
@@ -92,29 +117,109 @@ int solveMaze(int r, int c)
     }
     return 0;
 }
+void manualMode()
+{
+    int i = 0;
+    int j = 0;
+    char move;
+    printPlayerMaze();
+    while (playMaze[9][9] != 8)
+    {
+        scanf("%c", &move);
+        switch (move)
+        {
+        case 'w':
+            if (playMaze[i - 1][j] == 0 && i - 1 >= 0)
+            {
+                playMaze[i - 1][j] = 8;
+                playMaze[i][j] = maze[i][j];
+                printPlayerMaze();
+                i--;
+            }
+            else
+            {
+                printf("There is a wall\n");
+                printPlayerMaze();
+            }
+            break;
+        case 'a':
+            if (playMaze[i][j - 1] == 0 && j - 1 >= 0)
+            {
+                playMaze[i][j - 1] = 8;
+                playMaze[i][j] = maze[i][j];
+                printPlayerMaze();
+
+                j--;
+            }
+            else
+            {
+                printf("There is a wall\n");
+                printPlayerMaze();
+            }
+            break;
+        case 's':
+            if (playMaze[i + 1][j] == 0 && i + 1 >= 0)
+            {
+                playMaze[i + 1][j] = 8;
+                playMaze[i][j] = maze[i][j];
+                printPlayerMaze();
+                i++;
+            }
+            else
+            {
+                printf("There is a wall\n");
+                printPlayerMaze();
+            }
+            break;
+        case 'd':
+            if (playMaze[i][j + 1] == 0 && j + 1 >= 0)
+            {
+                playMaze[i][j + 1] = 8;
+                playMaze[i][j] = maze[i][j];
+                printPlayerMaze();
+                j++;
+            }
+            else
+            {
+                printf("There is a wall");
+                printPlayerMaze();
+            }
+
+            break;
+
+        default:
+            break;
+        }
+    }
+    printf("Congratulations you finish the maze");
+}
 
 int main(int argc, char const *argv[])
 {
-    printMaze();
+    char answer;
+    printf("Press 1 to play manual\n");
+    printf("Press 2 to play auto\n");
+    scanf("%c", &answer);
 
-    printf("Solucion: \n");
-    /* int i, j;
-    for (int i = 0; i < TAM; i++)
+    if (answer == '1')
     {
-        for (int j = 0; j < TAM; j++)
+        printf("You can move using W[UP] S[DOWN]  D[RIGHT] A[LEFT] \n");
+        manualMode();
+    }
+    if (answer == '2')
+    {
+        printf("The maze \n");
+        printMaze();
+        printf("Solucion: \n");
+        if (solveMaze(0, 0))
         {
-            solucion[i][j] = 0;
+            printSolucion();
         }
-    } */
+        else
+        {
+            printf("No solution");
+        }
 
-    if (solveMaze(0, 0))
-    {
-        printSolucion();
+        return 0;
     }
-    else
-    {
-        printf("No hay solucion");
-    }
-
-    return 0;
 }
